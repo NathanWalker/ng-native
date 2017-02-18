9b38f422e824e26bf635552fdc7388cfcccdb80c
Nathan Walker <walkerrunpdx@gmail.com>
2017-02-18 14:14:20 -0800

    setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from "@angular/core";
+import { isIOS } from 'platform';
+import { registerElement } from 'nativescript-angular';
+if (isIOS) {
+    registerElement('AudioPlot', () => require('nativescript-ezaudio').AudioPlot);
+}
 
 @Component({
     selector: "my-app",
