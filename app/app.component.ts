95b59dc0b5902431d1b68c5c84d4a933e11e2471
Nathan Walker <walkerrunpdx@gmail.com>
2017-02-18 13:40:26 -0800

    setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,17 +5,5 @@ import { Component } from "@angular/core";
     templateUrl: "app.component.html",
 })
 export class AppComponent {
-    public counter: number = 16;
     
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-    
-    public onTap() {
-        this.counter--;
-    }
 }
